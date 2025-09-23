aabb67c7805fe2e064ff1499e91540b6becbd688
Harshdeep Singh <143889275+Harshdeep-Singh2004@users.noreply.github.com>
2025-09-23 14:46:50 +0530

    Update PrivateConstructor.cpp

--- PrivateConstructor.cpp
+++ PrivateConstructor.cpp
@@ -23,3 +23,32 @@ int main() {
 
     return 0;
 }
+///////////////////////////////////////       FRIEND         ////////////////////////////////////////////////
+
+
+///////////////////////////////////////       STATIC         ////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+
+class Phone{
+private:
+    Phone(int x) {
+        volume = x;
+    }
+public:
+    int volume;
+    static Phone hello(int x);
+
+};
+
+Phone Phone::hello(int x) {
+    return Phone(x);
+}
+
+int main() {
+
+    Phone p = Phone::hello(12);
+    cout << p.volume << endl;
+
+    return 0;
+}
